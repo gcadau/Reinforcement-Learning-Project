31b576f90bbe9858f96f86e6c3a7d85544cf995e
gcad96 <48099775+gcad96@users.noreply.github.com>
2022-08-14 11:50:24 +0200

    implemented different discrepancy functions

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ def parse_args():
     parser.add_argument('--device', default='cpu', type=str, help='network device [cpu, cuda]') #da togliere?
     parser.add_argument('--training_algorithm', default='PPO', type=str, help='training algorithm [PPO, TRPO]')
     parser.add_argument('--initialPhi', default='fixed', type=str, help='initial values for phi [fixed, random]')
-    parser.add_argument('--normalize', default=False, action='store_true', help="normalize parametes search space to [0,4]")
+    parser.add_argument('--normalize', default=False, action='store_true', help='normalize parametes search space to [0,4]')
     parser.add_argument('--logspace', default=False, action='store_true', help="use a log space for standard deviations (makes senses only if 'normalize' is set to True)")
-    parser.add_argument('--budget', default=1000, type=int, help="Number of evaluations in the optimization problem (i.e.: number of samples from the distribution)")
-    parser.add_argument('--n_iterations', default=1, type=int, help="Number of iterations in SimOpt algorithm")
+    parser.add_argument('--budget', default=1000, type=int, help='Number of evaluations in the optimization problem (i.e.: number of samples from the distribution)')
+    parser.add_argument('--n_iterations', default=1, type=int, help='Number of iterations in SimOpt algorithm')
+    parser.add_argument('--T_first', default='max', type=str, help='T-firtst value in discrepancy function [max, min, fixed:<number>]')
 
     return parser.parse_args()
 
@@ -27,7 +28,7 @@ def main():
 
     simopt = SimOpt()
 
-    simopt.distribution_optimization(training_algorithm=args.training_algorithm, initPhi=args.initialPhi, normalize=args.normalize, logspace=args.logspace, budget=args.budget, n_iterations=args.n_iterations)
+    simopt.distribution_optimization(training_algorithm=args.training_algorithm, initPhi=args.initialPhi, normalize=args.normalize, logspace=args.logspace, budget=args.budget, n_iterations=args.n_iterations, T_first=args.T_first)
 
     #get opt values, recommend paramters (2 modi)
 
